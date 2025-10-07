724a412778892a5551b440d6942d89323dc5b70b
telaprolulasya <2300031910cseh@gmail.com>
2025-10-07 12:25:12 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-98-81-204-182.compute-1.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://ec2-44-244-220-171.us-west-2.compute.amazonaws.com:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://ec2-98-81-204-182.compute-1.amazonaws.com:8080/api/products/images/${imagePath}`;
+  return `http://ec2-44-244-220-171.us-west-2.compute.amazonaws.com:8080/api/products/images/${imagePath}`;
 };
